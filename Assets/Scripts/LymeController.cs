55bc4495106834151b8796c3e5bfd245817d2875
ku6ro9ro <ku6ro9ro@gmail.com>
2019-11-20 02:26:02 +0900

    MOBを目的地まで移動させる

--- Assets/Scripts/LymeController.cs
+++ Assets/Scripts/LymeController.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using AM1.Nav;
-
+using UnityEngine.AI;
 
 public class LymeController : MonoBehaviour {
+    public GameObject target;
+    NavMeshAgent agent;
+    private float arriveDistance = 3f;
+    private float followDistance = 2f;
 
 	// Use this for initialization
 	void Start () {
-		
+        agent = GetComponent<NavMeshAgent>();	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        agent.destination = target.transform.position;
+        if(agent.remainingDistance < arriveDistance)
+        {
+            agent.isStopped = true;
+            //animator
+        }else if(agent.remainingDistance > followDistance)
+        {
+            agent.isStopped = false;
+            //animator
+        }
 		
 	}
 }
