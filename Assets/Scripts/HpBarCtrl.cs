e2a2920c6c4e6d751c0dbb899d4a4c78d5206fbe
ku6ro9ro <ku6ro9ro@gmail.com>
2019-11-29 00:13:21 +0900

    HPバー実装 死亡時処理を作成

---
+++ Assets/Scripts/HpBarCtrl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpBarCtrl : MonoBehaviour {
+
+    public Animator targetAnimator;
+
+    Slider life;
+    float damage = 0.5f;
+    Animation anim;
+
+    // Use this for initialization
+    void Start () {
+        life = GameObject.Find("Slider").GetComponent<Slider>();
+        life.maxValue = 100;
+        life.value = 100;
+    }
+
+
+
+    void Update()
+    {
+        if(life.value <= 0)
+        {
+            targetAnimator.SetTrigger("down");
+            targetAnimator.SetTrigger("end");
+            life.value = 100;
+            
+        }
+
+        // HPゲージに値を設定
+        life.value -= damage;
+    }
+}
